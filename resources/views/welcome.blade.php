20c2f50cc18113787a9e438935193313f509ab3b
Eder Díaz <ederchrono@gmail.com>
2016-11-02 13:46:03 -0600

    improved style of the errors on main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>URL Shortener</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -47,7 +47,7 @@
                 {{ Form::close() }}
 
                 @if (count($errors) > 0)
-                    <div class="alert alert-danger">
+                    <div class="errors">
                         <ul>
                             @foreach ($errors->all() as $error)
                                 <li>{{ $error }}</li>
