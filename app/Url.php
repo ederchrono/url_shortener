7d13d4469eba4cb50037668061abae61b9389692
Eder Díaz <ederchrono@gmail.com>
2016-11-02 12:10:01 -0600

    added hashids library https://github.com/vinkla/laravel-hashids to change numerical ids to hashed string

--- app/Url.php
+++ app/Url.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
+use Vinkla\Hashids\Facades\Hashids;
 
 class Url extends Model
 {
@@ -10,6 +12,6 @@ class Url extends Model
 
     public static function shortenURL($long_url)
     {
-        return "http://edr.com/".bcrypt($long_url);
+        return "http://edr.com/".Hashids::encode(rand(0,100));
     }
 }
--- config/app.php
+++ config/app.php
@@ -180,6 +180,8 @@ return [
 
 
         Collective\Html\HtmlServiceProvider::class,
+
+        Vinkla\Hashids\HashidsServiceProvider::class
     ],
 
     /*
@@ -230,6 +232,7 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Hashids' => Vinkla\Hashids\Facades\Hashids::class
     ],
 
 ];
